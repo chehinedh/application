a21221291daeedd8029129d6ba525d9747de94f3
chehinedh <chehine.dhemaied@esprit.tn>
2023-02-12 16:55:16 +0100

    🚧 passing method references

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import React, { Component, useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-
-function App()  {
+function App() {
   const [person1, setPerson1] = useState({ nom: "chehine", age: "23" });
   const [person2, setPerson2] = useState({ nom: "houssem", age: "26" });
 
-  const switchHandler = () => {
-    console.log('was clicked!');
+  const switchHandler = (newName, person) => {
+    if (person === 1) {
+      setPerson1({ ...person1, nom: newName });
+    } else {
+      setPerson2({ ...person2, nom: newName });
+    }
   };
 
   return (
     <div className="App">
       <h1>Hi, I'm a React App </h1>
       <p>This is really working! </p>
-      <button onClick={switchHandler}>Switch Name</button>
-      <Person name={person1.nom} age={person1.age}>
+      <button onClick={() => switchHandler("Chehine", 1)}>Switch Name</button>
+      <Person
+        name={person1.nom}
+        age={person1.age}
+        click={() => switchHandler("Chehine", 1)}
+      >
         My Hobbies: Boxing
       </Person>
-      <Person name={person2.nom} age={person2.age} />
+      <Person
+        name={person2.nom}
+        age={person2.age}
+        click={() => switchHandler("Houssem", 2)}
+      />
     </div>
   );
 }
