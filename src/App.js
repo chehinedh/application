6aa4aa8ae9e753b979393622414d26f6a5174827
chehinedh <chehine.dhemaied@esprit.tn>
2023-02-12 16:15:29 +0100

    ✅ using state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-function App() {
+
+function App()  {
+  const [person1, setPerson1] = useState({ nom: "chehine", age: "23" });
+  const [person2, setPerson2] = useState({ nom: "houssem", age: "26" });
+
+  const switchHandler = () => {
+    console.log('was clicked!');
+  };
+
   return (
     <div className="App">
       <h1>Hi, I'm a React App </h1>
       <p>This is really working! </p>
-      <Person name="chehine" age="23"  ></Person>
-      <Person name="houssem" age="26" />
+      <button onClick={switchHandler}>Switch Name</button>
+      <Person name={person1.nom} age={person1.age}>
+        My Hobbies: Boxing
+      </Person>
+      <Person name={person2.nom} age={person2.age} />
     </div>
   );
 }
